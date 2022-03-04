233c63af944b6fb7894713592148a39d1bb1b64b
Camilo Quiroga <decimo23hotmail.com>
2022-03-04 10:49:51 -0500

    buscador

---
+++ app/Inmueble.php
@@ -0,0 +1,8 @@
+use Illuminate\Database\Eloquent\Model;
+
+class Inmueble extends Model
+{
+  protected $fillable = [
+  'departamento', 'minicipio', 'barrio', 'zona', 'tipoinmueble', 'gestion','alcobas','baños','valor'
+  ];
+}
